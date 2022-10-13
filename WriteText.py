fbd4e0bebbcac8b53ae47c99c9ea715336a20dbe
AppTimDev <apptimdev@gmail.com>
2022-10-13 16:08:38 +0800

    support adding chinese words

--- WriteText.py
+++ WriteText.py
@@ -1,6 +1,8 @@
 """
 Usage:
 Write some text on an image
+
+Font used to support adding chinese words: Microsoft JhengHei font family 
 """
 from PIL import Image, ImageDraw, ImageFont
 
@@ -10,12 +12,13 @@ print(f"Image size: {w} x {h}")
 
 #User-defined variables
 text_pos_x = w//4
-text_pos_y = h-36
-text = "Hello!"
+text_pos_y = h-60
+text = "Hello! 大家好!!"
 
 d1 = ImageDraw.Draw(img)
-d1.text((text_pos_x, text_pos_y), text, fill=(255, 255, 255))
+myFont = ImageFont.truetype('msjhbd.ttc', 20) #font-size
+d1.text((text_pos_x, text_pos_y), text, font=myFont, fill=(255, 255, 255)) #text color white
 
 #show and save image
-#img.show()
+img.show()
 img.save('output.png', 'png')
